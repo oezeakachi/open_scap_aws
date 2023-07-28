176a11426f09df97772eab5f134ace0f92be9b29
Obinna Ezeakachi <oezeakac@oezeakac-thinkpadt14gen3.remote.csb>
2023-07-28 18:39:22 +0100

    Lambda runs

--- main.tf
+++ main.tf
@@ -174,13 +174,7 @@ resource "aws_lambda_function" "ProcessSCAPScanResults" {
   depends_on    = [aws_cloudwatch_log_group.lambda_log_group]
 }
 
-resource "aws_lambda_permission" "S3InvokeLambdaPermission" {
-  statement_id  = "AllowExecutionFromS3"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.ProcessSCAPScanResults.arn
-  principal     = "s3.amazonaws.com"
-  source_arn    = aws_s3_bucket.SCAPScanResultsBucket.arn
-}
+
 
 resource "aws_iam_role" "SCAPEC2InstanceRole" {
   name = "SCAPEC2InstanceRole"
@@ -314,12 +308,17 @@ resource "aws_s3_bucket_notification" "SCAPScanResultsBucketNotification" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.ProcessSCAPScanResults.arn
     events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "AWSLogs/"
-    filter_suffix       = ".log"
-
   }
 
-  depends_on = [aws_lambda_permission.S3InvokeLambdaPermission]
+} 
+
+
+resource "aws_lambda_permission" "S3InvokeLambdaPermission" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.ProcessSCAPScanResults.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.SCAPScanResultsBucket.arn
 }
 
 resource "aws_ssm_parameter" "EnableSecurityHubFindingsParameter" {
